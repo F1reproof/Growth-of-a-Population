780d10a4687787b34170d0b5b403fd10047a1f37
F1reproof <lana.gergel@yandex.ru>
2022-10-03 21:24:16 +0300

    result 2.0v

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function nbYear(p0, percent, aug, p,count=0) {
-    return  p0>=p ? count :(count+=1, nbYear(p0=(p0+ p0* percent/100 +aug),percent,aug,p,count))
+function nbYear(p0, percent, aug, p) {
+
+    for (var years = 0; p0 < p; years++) {
+        p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+    }
+    return years
 }
